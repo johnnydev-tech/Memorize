60297a6d7f592382ea05c1576c0b0fda2657c9ec
JohnnyFdev <johnnyfreirejh@hotmail.com>
2022-11-25 22:59:31 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Johnny Freire on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
